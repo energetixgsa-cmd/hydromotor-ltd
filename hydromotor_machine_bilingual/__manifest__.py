dace661a9bbbcfd3b9f77ff6809134f05eef5f87
energetixgsa-cmd <energetixgsa@gmail.com>
2026-09-20 15:45:00 +0300

    Update __manifest__.py

--- hydromotor_machine_bilingual/__manifest__.py
+++ hydromotor_machine_bilingual/__manifest__.py
@@ -1,5 +1,6 @@
 {
     'name': 'Hydromotor Machine Catalog BG / EN',
+    'author': 'Hydromotor',
     'version': '19.0.1.0.0',
     'license': 'LGPL-3',
     'depends': ['hydromotor_machine_sales'],
