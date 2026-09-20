6ca8920a13119a438dcc4c097e1f5e99df60731d
energetixgsa-cmd <energetixgsa@gmail.com>
2026-09-20 15:31:24 +0300

    Add files via upload

---
+++ hydromotor_machine_bilingual/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hydromotor Machine Catalog BG / EN',
+    'version': '19.0.1.0.0',
+    'license': 'LGPL-3',
+    'depends': ['hydromotor_machine_sales'],
+    'data': ['data/fields.xml'],
+    'post_init_hook': 'post_init_hook',
+    'uninstall_hook': 'uninstall_hook',
+    'installable': True,
+    'application': False,
+}
