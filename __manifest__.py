c381ca6df0d5f37162fc0546a69df7685a7594fd
energetixgsa-cmd <energetixgsa@gmail.com>
2026-09-22 20:29:19 +0300

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,25 @@
 {
-    'name': 'Hydromotor - Комплекти части за машини',
-    'version': '19.0.1.0.0',
-    'summary': 'Комплекти резервни части по машина и добавяне към сервизна оферта',
-    'author': 'Hydromotor',
-    'license': 'LGPL-3',
-    'depends': ['hydromotor_service', 'sale_management'],
-    'data': [
-        'models/definitions.xml',
-        'security/access.xml',
-        'views/views.xml',
+    "name": "Hydromotor TREMOL Fiscal",
+    "version": "19.0.1.0.1",
+    "summary": "TREMOL M23 fiscal receipts from Odoo invoices through ZFPLabServer",
+    "author": "Hydromotor",
+    "license": "LGPL-3",
+    "depends": ["account", "web"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_config_settings_views.xml",
+        "wizard/tremol_receipt_wizard_views.xml",
+        "views/account_move_views.xml",
     ],
-    'application': False,
-    'installable': True,
-    'auto_install': False,
-    'pre_init_hook': 'pre_init_hook',
+    "assets": {
+        "web.assets_backend": [
+            "hydromotor_tremol_fiscal/static/src/js/tremol_bridge.js",
+            "hydromotor_tremol_fiscal/static/src/xml/tremol_bridge.xml",
+        ],
+    },
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
+
+
